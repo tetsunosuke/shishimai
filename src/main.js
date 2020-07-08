3acb2d9f11be1aea121ed147c5f3962f92befea4
ITO Tetsunosuke <tetsunosuke.ito@photocreate.co.jp>
2020-07-08 16:00:23 +0900

    ライブラリ分割等

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// TODO: 閉じたときの処理がまだおかしい
 const path = require("path");
 const electron = require('electron');
 const app = electron.app;
@@ -10,43 +11,53 @@ const ipc = require('electron').ipcMain
 const Store = require('electron-store');
 const store = new Store({});
 const kinrouUrl = "https://kinrou.sas-cloud.jp/kinrou";
-const conf = store.get("kinrou");
+const kinrouLib = require("../lib/kinrou.js");
 
 const debug = /--debug/.test(process.argv[2])
+let conf = store.get("kinrou", null);
+if (debug) {
+    conf.password = "xxxxxx";
+}
 
 // puppeteerを使うときはこのタイミングでinitializeする
 pie.initialize(app);
 
-let mainWindow = null;
+var mainWindow = null;
 
 require("update-electron-app");
 const logger = require("electron-log");
 // Render側に渡すためのメッセージ文字列
 var text = "";
-// main.htmlからsendを受け取ったらrecieveを返すイベント
+// main.htmlからsendを受け取ったらrecieveとともにテキストを返すイベント
 ipc.on('send', function(event) {
     console.log("receive send message", text);
     event.sender.send('receive', text)
 })
+
 
 // メニューに関する情報
 const template = [
     {
         "label":"メニュー",
         "submenu": [{
-            "label": "ログイン情報", 
-            "click": async () => {
-                console.log("TODO: open config.html");
-            }
+            "label": "ログイン情報",
+            "click": function(m,b,e) {
+                // logger.log(m, b, e);
+                kinrouLib.showConfig(conf);
+            },
+            "accelerator":  "CmdOrCtrl+L"
         }, {
-            "label": "パスワード変更", 
-            "click": async () => {
-                console.log("TODO: open config.html");
-            }
+            "label": "パスワード変更",
+            "click": function(m,b,e) {
+                // logger.log(m, b, e);
+                kinrouLib.showConfig(conf);
+            },
+            "accelerator":  "CmdOrCtrl+P"
         }, {
-            "label": "実行", 
-            "click": async () => {
-                dakoku();
+            "label": "実行",
+            "click": function(m,b,e) {
+                // logger.log(m, b, e);
+                kinrouLib.dakoku(mainWindow)
             },
             "accelerator":  "CmdOrCtrl+R"
         }]
@@ -54,12 +65,9 @@ const template = [
 ];
 
 const initialize = async () => {
-    console.info("initialize");
-    logger.log("log");
-    logger.info("info");
+    logger.log("initialize");
     makeSingleInstance();
 
-    //async function createWindow() {
     const createWindow = async () => {
         const windowOptions = {
             width: 1080,
@@ -71,9 +79,10 @@ const initialize = async () => {
         };
 
         mainWindow = new BrowserWindow(windowOptions);
-        // 一旦デフォルトページを呼び出す作りにしてみる
-        dakoku();
-        // mainWindow.loadURL(path.join('file://', __dirname, '/index.html'));
+        if (debug) {
+            mainWindow.webContents.openDevTools()
+        }
+        console.log("mainWindow is initialized");
 
         mainWindow.on("closed", () => {
             mainWindow = null;
@@ -81,16 +90,27 @@ const initialize = async () => {
     };
 
     app.on('ready', () => {
-        console.log("ready");
+        logger.log("ready");
+        createWindow();
         const menu = Menu.buildFromTemplate(template);
         Menu.setApplicationMenu(menu);
-        createWindow();
-        if (debug) {
-            mainWindow.webContents.openDevTools()
+        if (conf === null) {
+            console.info("show config with config is null");
+            kinrouLib.showConfig(conf);
+            return;
         }
+
+        // 設定があれば打刻処理を実施
+        kinrouLib.dakoku(mainWindow);
+    });
+
+    app.on("browser-window-created", (o) => {
+        // こっちで処理を開始すべき？
+        logger.log("browser-window-created");
     });
 
     app.on('window-all-closed', () => {
+        console.info("platformがdarwinの場合にapp.quitが呼ばれないが良いのか？");
         if (process.platform != 'darwin') {
             app.quit();
         }
@@ -102,6 +122,7 @@ const initialize = async () => {
         }
     });
 }
+
 
 function makeSingleInstance () {
     if (process.mas) {
@@ -120,58 +141,6 @@ function makeSingleInstance () {
     })
 }
 
-const dakoku = async () => {
-    browser = await pie.connect(app, puppeteer);
-    const page = await pie.getPage(browser, mainWindow);
-    mainWindow.loadURL(kinrouUrl + "/kojin/");
-
-    // ログイン。法人コード、社員コード、パスワードを入力し、ログインボタンを押します
-    await page.waitForSelector("[name=houjinCode]");
-    await page.type('[name=houjinCode]', conf.houjinCode);
-    await page.type('[name=userId]',     conf.userId);
-    await page.type('[name=password]',   conf.password);
-    // 次のページが読み込まれるまで待つという意味です
-    await Promise.all([
-        page.waitForNavigation(),
-       page.click("#bt")
-    ]);
-
-    // 打刻
-    try {
-        if (!debug) {
-            await page.click("[name='dakoku']");
-        }
-    } catch (e) {
-        // ログインエラー
-        // TODO: 他にも通信タイムアウトエラーが出るケースがあることがわかっている
-        await Promise.all([
-            page.waitForSelector("#error"),
-            elm = await page.$("#error")
-        ]);
-        text = await page.evaluate(elm => elm.textContent, elm);
-        await mainWindow.loadURL('file://' + __dirname + '/main.html');
-        return;
-    }
-
-    const d = new Date();
-    const year = dateformat(d, 'yyyy');
-    const month = dateformat(d, 'mm');
-    const kijunDate = dateformat(d, 'yyyymmdd');
-
-    await page.goto(`${kinrouUrl}/dakokuList/index?syainCode=${conf.userId}&year=${year}&month=${month}&kijunDate=${kijunDate}`);
-
-    let elms = await page.$$(".dakoku-all-list tr");
-    let dakokuType;
-    let dakokuTime;
-    text = "最後の打刻は：";
-    elm = await page.$(`.dakoku-all-list tr:nth-of-type(${elms.length-1}) td:nth-of-type(3)`);
-    dakokuType = await page.evaluate(elm => elm.textContent, elm);
-    elm = await page.$(`.dakoku-all-list tr:nth-of-type(${elms.length-1}) td:nth-of-type(4)`);
-    dakokuTime = await page.evaluate(elm => elm.textContent, elm);
-    // ここで文字列を保持しておき、index.htmlに読み込ませる
-    text = `最後の打刻は ${dakokuTime} (${dakokuType})`;
-    await mainWindow.loadURL('file://' + __dirname + '/main.html');
-}
 
 process.on('uncaughtException', function(err) {
   logger.error('electron:event:uncaughtException');
@@ -182,4 +151,3 @@ process.on('uncaughtException', function(err) {
 
 // 実行開始
 initialize();
-
